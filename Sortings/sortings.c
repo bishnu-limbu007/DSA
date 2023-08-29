88da1976ad0135426a055e144911b7623372e850
bishnulimbu <subashlimbu987654321@gmail.com>
2023-08-29 07:19:44 +0545

    insertion sort done

--- Sortings/sortings.c
+++ Sortings/sortings.c
@@ -16,16 +16,16 @@ void insertionSort(int arr[], int n){
 int main(){
   int arr[] = {9,8,7,6,5,4,3,2,1,0};
   int i;
-  printf("Before sortings....");
+  printf("Before sortings....\n");
 
   for (i=0 ; i<10 ; i++){
-    printf("%d", arr[i]);
+    printf("%d ", arr[i]);
   }
-  printf("After sortings....");
+  printf("\nAfter sortings....\n");
   insertionSort(arr,10);
   
   for (i=0 ; i<10 ; i++){
-    printf("%d", arr[i]);
+    printf("%d ", arr[i]);
   }
 
   return 0;
