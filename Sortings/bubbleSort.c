6bf6c430582d7dcf65f9455f6943b384b0fd6e60
bishnulimbu <subashlimbu987654321@gmail.com>
2023-08-30 20:47:09 +0545

    bug fixed

--- Sortings/bubbleSort.c
+++ Sortings/bubbleSort.c
@@ -5,7 +5,7 @@
 void bubbleSort(int arr[],int n){
   int temp;
   for(int i=0 ; i<10 ; i++){
-    for(int j=0 ; j<10 ; j++){
+    for(int j=0 ; j< n - i - 1 ; j++){
       if(arr[j] > arr[j+1]){
         temp = arr[j];
         arr[j] = arr[j+1];
@@ -28,5 +28,5 @@ int main(){
   for(int i=0 ; i<10 ; i++){
     printf("%d ", arr[i]);
   }
-
+  return 0;
 }
