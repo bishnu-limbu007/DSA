890268e2df28384f8198126514395b4920214033
bishnulimbu <subashlimbu987654321@gmail.com>
2023-08-31 20:40:02 +0545

    1st

--- stack.c
+++ stack.c
@@ -7,4 +7,20 @@
 int top;
 int arr[MAX];
 
+int main(){
+  int n;
+  char ch;
+  scanf("%c", &ch);
+  while(1){
+    switch(ch){
+      case 1:
+      push();
+      break;
+      case 2:
+      pop();
+      break;
+        case
+    }
+  }
+}
 
