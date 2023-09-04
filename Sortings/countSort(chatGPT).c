0bae88f060d069777badbc6493b2f52bd7bdfa0c
bishnulimbu <subashlimbu987654321@gmail.com>
2023-09-04 07:17:53 +0545

    count sortings

---
+++ Sortings/countSort(chatGPT).c
@@ -0,0 +1,59 @@
+
+#include <stdio.h>
+
+// Function to find the maximum value in the input array
+int findMax(int arr[], int n) {
+    int max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+// Function to perform counting sort
+void countingSort(int arr[], int n) {
+    int max = findMax(arr, n);
+    int count[max + 1]; // Create a counting array to store frequency
+
+    // Initialize the counting array with all zeros
+    for (int i = 0; i <= max; i++) {
+        count[i] = 0;
+    }
+
+    // Count the frequency of each element in the input array
+    for (int i = 0; i < n; i++) {
+        count[arr[i]]++;
+    }
+
+    // Reconstruct the sorted array from the counting array
+    int index = 0;
+    for (int i = 0; i <= max; i++) {
+        while (count[i] > 0) {
+            arr[index++] = i;
+            count[i]--;
+        }
+    }
+}
+
+int main() {
+    int arr[] = {8, 4, 3, 6, 5, 9, 2, 3, 7, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original Array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    countingSort(arr, n);
+
+    printf("Sorted Array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
--- Sortings/countSort.c
+++ Sortings/countSort.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 #define MAX 10
-#define HIGEST 9
-
+#define HIGHEST 9
 
 int higest(int arr[], int n){
   int i, max=arr[0];
@@ -10,28 +9,24 @@ int higest(int arr[], int n){
     if(max<arr[i])
       max=arr[i];
   }
-return max;
+  return max;
 }
 
 void countSort(int arr[], int n){
   int temp[MAX];
-  int count[HIGEST];
+  int count[HIGHEST];
   int max=higest(arr, n);
   int i;
-
-
+  for(i=0;i<n;i++){
+    count[arr[i]]++;
+  }
+  for(i=1;i<=max;i++)
+    count[i]+=count[i-1];
+  for(i=n-1;i>=0;i--)
+    temp[--count[arr[i]]] = arr[i];
+  for(i=0;i<n;i++)
+    arr[i] = temp[i];
 }
-
-
-
-
-
-
-
-
-
-
-
 
 int main(){
   int arr[MAX] = {8,4,3,6,5,9,2,3,7,8};
@@ -39,6 +34,7 @@ int main(){
   printf("Before sortings.....\n");
   for (i=0;i<MAX;i++)
     printf("%d ", arr[i]);
+  countSort(arr, MAX);
   printf("After sortings.....\n");
   for (i=0;i<MAX;i++)
     printf("%d ", arr[i]);
