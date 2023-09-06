f7a816971e989493ed8fc6de17e7febc71b6b451
bishnulimbu <subashlimbu987654321@gmail.com>
2023-09-06 09:07:30 +0545

    completed ....probably

--- practise/sortinp/selection.c
+++ practise/sortinp/selection.c
@@ -1,16 +1,28 @@
 #include<stdio.h>
 
-sort(){                        //selection sort
-
+void sort(int arr[], int n){                        //selection sort
+  int smallest, temp, i, j;
+  for(i=0 ; i<n-1 ; i++){
+    smallest = i;
+    for(j=i+1 ; j<10 ; j++){
+      if(smallest>arr[j]){
+        smallest = j;
+      }      
+    }
+    temp = arr[i];
+    arr[i] = arr[smallest];
+    arr[smallest] = temp;
+  }
 }
 
 
 
 int main(){
-  int i;
+  int i, n;
   int arr[]={10,9,8,7,6,5,4,3,2,1};
   for(i=0 ; i<9 ; i++)
     printf("%d ", arr[i]);
+  printf("\n");
   sort(arr, n);
   for(i=0 ; i<9 ; i++)
     printf("%d ", arr[i]);
