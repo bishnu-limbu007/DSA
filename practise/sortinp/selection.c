1fdbd9092823207c018f88e74a9002ac39d17037
bishnulimbu <subashlimbu987654321@gmail.com>
2023-09-06 08:55:46 +0545

    practising

---
+++ practise/sortinp/selection.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+sort(){                        //selection sort
+
+}
+
+
+
+int main(){
+  int i;
+  int arr[]={10,9,8,7,6,5,4,3,2,1};
+  for(i=0 ; i<9 ; i++)
+    printf("%d ", arr[i]);
+  sort(arr, n);
+  for(i=0 ; i<9 ; i++)
+    printf("%d ", arr[i]);
+  return 0;
+}
