c3a17a1641ae2314e20038e2a3078ccf460a4bc5
bishnulimbu <subashlimbu987654321@gmail.com>
2023-09-10 20:56:45 +0545

    okay

--- Sortings/countSort.c
+++ Sortings/countSort.c
@@ -14,7 +14,7 @@ int higest(int arr[], int n){
 
 void countSort(int arr[], int n){
   int temp[MAX];
-  int count[HIGHEST];
+  int count[HIGHEST+1];
   int max=higest(arr, n);
   int i;
   for(i=0;i<n;i++){
@@ -29,7 +29,7 @@ void countSort(int arr[], int n){
 }
 
 int main(){
-  int arr[MAX] = {8,4,3,6,5,9,2,3,7,8};
+  int arr[MAX] = {9,8,7,6,5,4,3,2,1,0};
   int i;
   printf("Before sortings.....\n");
   for (i=0;i<MAX;i++)
