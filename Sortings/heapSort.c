4786940c964c0c24057e2deee581c42d08cd4328
bishnulimbu <subashlimbu987654321@gmail.com>
2023-09-12 18:16:44 +0545

    heap sort in c

--- Sortings/heapSort.c
+++ Sortings/heapSort.c
@@ -1,23 +1,62 @@
-#include<stdio.h>
+#include <stdio.h>
 
+// Function to heapify a subtree with the root at given index
+void heapify(int arr[], int n, int i) {
+    int largest = i; // Initialize largest as root
+    int left = 2 * i + 1; // Left child
+    int right = 2 * i + 2; // Right child
 
+    // If left child is larger than root
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
 
+    // If right child is larger than largest so far
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
 
-int main(){
-  int arr[] = {9,8,7,6,5,4,3,2,1,0};
-  int i;
-  printf("Before sortings....\n");
+    // If largest is not the root
+    if (largest != i) {
+        // Swap arr[i] and arr[largest]
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
 
-  for (i=0 ; i<10 ; i++){
-    printf("%d ", arr[i]);
-  }
-  printf("\nAfter sortings....\n");
-  heapSort(arr,10);
+        // Recursively heapify the affected sub-tree
+        heapify(arr, n, largest);
+    }
+}
 
-  for (i=0 ; i<10 ; i++){
-    printf("%d ", arr[i]);
-  }
+// Main function to perform heap sort
+void heapSort(int arr[], int n) {
+    // Build heap (rearrange array)
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
 
-  return 0;
+    // One by one extract an element from the heap
+    for (int i = n - 1; i > 0; i--) {
+        // Move current root to end
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
 
+        // call max heapify on the reduced heap
+        heapify(arr, i, 0);
+    }
+}
+
+int main() {
+    int arr[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    heapSort(arr, n);
+
+    printf("\nSorted array (ascending order): ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
 }
