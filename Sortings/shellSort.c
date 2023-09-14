74fb68367870ebc0218367c83fe3369909526240
bishnulimbu <subashlimbu987654321@gmail.com>
2023-09-14 18:55:41 +0545

    daf

--- Sortings/shellSort.c
+++ Sortings/shellSort.c
@@ -1,17 +1,5 @@
 #include <stdio.h>
 
-void shellSort(int arr[], int n){
-  int gap, temp, i, j;                     //half size of total interger
-  for(gap=n/2;gap>0;gap= gap/2){
-    for(i=gap;i<n;i++){
-      temp= arr[i];                        //temp 1st item store
-      for(j=i-gap; j>=0 && arr[j]>temp; j=j-gap)
-        arr[j+gap] = arr[j];
-      arr[j+gap] = temp;
-    } 
-  }
-}
-
 
 
 int main(){
