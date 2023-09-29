0b98c2df5d061b4b545c74c519194a5ba9ff93f0
bishnulimbu <subashlimbu987654321@gmail.com>
2023-09-29 13:19:49 +0545

    dfads

---
+++ PractiseForExam/Copy1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct Node{
+  int data;
+  struct Node *next;
+};
+
+void display(struct Node*ptr){
+  while(ptr!=NULL)
+    printf("elemets: %d\n" ptr->data);
+}
+int main(){
+  struct Node *head;
+  struct Node *second;
+  struct Node *third;
+  struct Node *fourth;
+  head = (struct Node*) malloc (sizeof(struct Node));
+  second = (struct Node*) malloc (sizeof(struct Node));
+  third = (struct Node*) malloc (sizeof(struct Node));
+  fourth = (struct Node*) malloc (sizeof(struct Node));
+  head->data = 100;
+  head->next = second;
+  second->data = 101;
+  second->next = third;
+  third->data = 103;
+  third->next = fourth;
+  fourth->data = 104;
+  fourth->next = NULL;
+  display(head);
+  return 0;
+}
