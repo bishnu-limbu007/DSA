47aa531e0bc4510dc86adc8e6a2a1fb115be11cd
bishnulimbu <subashlimbu987654321@gmail.com>
2023-10-01 11:25:09 +0545

    dfalksdjf

---
+++ PractiseForExam/Stack2.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#define MAX 10
+
+typedef struct{
+  int data;
+  int top[MAX];
+}stack;
+
+void init(stack s){
+  s->top=-1;
+}
+void push(stack s,int value){
+  if(s->top==MAX-1){
+    printf("stack overflow");
+  }
+  else{
+    s->data[++s->top]= value;
+  }
+}
+void pop(stack s){
+  if(s->top==-1){
+    printf("stack underflow");
+  }
+  else{
+    s->data[s->top--];
+  }
+}
+void display(stack s){
+  for(int i =0;i<s->top;i++){
+    printf("data: %d", s.data[i]);
+  }
+}
+int main(){
+  stack s;
+  init(&s);
+  push(&s, 1);
+  push(&s, 2);
+  push(&s, 3);
+  pop(&s);
+  display(&s);
+  return 0;
+}
