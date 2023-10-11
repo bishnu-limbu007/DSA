abdbbf66f5bda608ba142974828275bbf2ad3aad
bishnulimbu <subashlimbu987654321@gmail.com>
2023-10-11 19:18:30 +0545

    adsfads

--- PractiseForExam/Linkedlist.c
+++ PractiseForExam/Linkedlist.c
@@ -1,35 +1,78 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-struct Node{
-  int data;
-  struct Node *next;
+// struct Node
+// {
+//     int data;
+//     struct Node *next;
+// };
+
+struct Node {
+    int data;
+    Struct Node* next;
+    Struct Node* prev;
 };
 
-void traversal(struct Node *ptr){
-  while(ptr!=NULL){
- printf("element : %d\n",ptr->data);
-  ptr=ptr->next;
-  }
+void linkedListTraversal(struct Node *head){
+    struct Node *ptr = head;
+    do{
+        printf("Element is %d\n", ptr->data);
+        ptr = ptr->next;
+    }while(ptr!=head);
+}
+
+struct Node * insertAtFirst(struct Node *head, int data){
+    struct Node * ptr = (struct Node *) malloc(sizeof(struct Node));
+    ptr->data = data;
+
+    struct Node * p = head->next;
+    while(p->next != head){
+        p = p->next;
+    }
+    // At this point p points to the last node of this circular linked list
+
+    p->next = ptr;
+    ptr->next = head;
+    head = ptr;
+    return head;
+
 }
 
 int main(){
-  struct Node * head;
-  struct Node * second;
-  struct Node * third;
-  head = (struct Node*) malloc(sizeof(struct Node));
-  second = (struct Node*) malloc(sizeof(struct Node));
-  third = (struct Node*) malloc(sizeof(struct Node));
-  
-  head->data =7;
-  head->next = second;
+    
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+    struct Node *fourth;
 
-  second->data =11;
-  second->next = third;
+    // Allocate memory for nodes in the linked list in Heap
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    fourth = (struct Node *)malloc(sizeof(struct Node));
 
-  third->data =99;
-  third->next = NULL;
+    // Link first and second nodes
+    head->data = 4;
+    head->next = second;
 
-  traversal(head);
-  return 0;
+    // Link second and third nodes
+    second->data = 3;
+    second->next = third;
+
+    // Link third and fourth nodes
+    third->data = 6;
+    third->next = fourth;
+
+    // Terminate the list at the third node
+    fourth->data = 1;
+    fourth->next = head;
+
+    printf("Circular Linked list before insertion\n");
+    linkedListTraversal(head);
+    head = insertAtFirst(head, 54);
+    head = insertAtFirst(head, 58);
+    head = insertAtFirst(head, 59);
+    printf("Circular Linked list after insertion\n");
+    linkedListTraversal(head);
+    return 0;
 }
